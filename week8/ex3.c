da444c5af968e84a288ec31cd179d59145608cae
mirnanoukari <mirnaalnoukari99@gmail.com>
2020-10-07 13:24:44 +0300

    first commit

--- week8/ex3.c
+++ week8/ex3.c
@@ -1,16 +1,18 @@
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
+
 
 int main() {
     void *ptr[10];
+    int s = 10 * 1024 * 1024;
     for (int i = 0; i < 10; i++) {
-        ptr[i] = malloc(10 * 1024 * 1024);
+        ptr[i] = malloc(s);
         if (ptr[i] != NULL)
-            memset(ptr[i], 0, 10 * 1024 * 1024);
+            memset(ptr[i], 0, s);
         sleep(1);
     }
-    // No need to free the memory, it's just good practice.
+    // we don't need to free the memory, but it's done for good practice.
     for (int i = 0; i < 10; i++)
         free(ptr[i]);
     return 0;
--- week8/ex4.c
+++ week8/ex4.c
@@ -7,24 +7,27 @@
 
 int main() {
     void *ptr[10];
-    struct rusage res;
+    struct rusage rs;
+    int s=10 * 1024 * 1024;
 
     for (int i = 0; i < 10; i++) {
-        ptr[i] = malloc(10 * 1024 * 1024);
+        ptr[i] = malloc(s);
         if (ptr[i] != NULL)
-            memset(ptr[i], 0, 10 * 1024 * 1024);
+            memset(ptr[i], 0, s);
 
-        getrusage(RUSAGE_SELF, &res);
-        printf("%s: %ld\n", "maximum resident set size", res.ru_maxrss);
+        getrusage(RUSAGE_SELF, &rs);
+        printf("%s: %ld\n", "Maximum resident set size", rs.ru_maxrss);
 
         sleep(1);
     }
 
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 10; i++){
         free(ptr[i]);
-    printf("Memory freed\n");
-    getrusage(RUSAGE_SELF, &res);
-    printf("%s: %ld\n", "maximum resident set size", res.ru_maxrss);
+        }
+        
+    printf("Memory is freed\n");
+    getrusage(RUSAGE_SELF, &rs);
+    printf("%s: %ld\n", "Maximum resident set size", rs.ru_maxrss);
         
     return 0;
 }
