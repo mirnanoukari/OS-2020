117f3e73f84b85306e6d095b6520024b83379687
mirnanoukari <mirnaalnoukari99@gmail.com>
2020-10-14 23:34:26 +0300

    complet commit

--- week9/ex1.c
+++ week9/ex1.c
@@ -1,64 +1,69 @@
-#include <limits.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
 #include <string.h>
 
-#define MAX_PAGES 1000
-#define AGE_LIM 32
+#define AGE_LIMIT 32 //32 bit by page age
+#define MAX_PAGES 999
 
-unsigned int page_frames_count;
-unsigned int page_counter[MAX_PAGES]; // using static storage whenever possible.
-unsigned int *page_table;
-int hits, misses;
+unsigned int *page_table; //page_table is an array of indexes of pages
+unsigned int PageCounter[MAX_PAGES];
+unsigned int pframesCount;
+int hits=0, misses=0;
 
 int main(int argc, char *argv[]) {
     if (argc <= 1) {
-        fprintf(stderr,
-                "The number of page frames needs to be passed. Exiting.\n");
+        fprintf(stderr,"Page frames number is not passed.\n");
         return 1;
     }
 
     FILE *input = fopen("Lab 09 input.txt", "r");
 
     if (input == NULL) {
-        fprintf(stderr, "Error opening input file. Exiting.\n");
+        fprintf(stderr, "Cannot open input file.\n");
         return 2;
     }
-
-    page_frames_count = atoi(argv[1]);
-    page_table = (unsigned int *)calloc(page_frames_count, sizeof(int));
-
+    pframesCount = atoi(argv[1]);
+    page_table = (unsigned int *)calloc(pframesCount, sizeof(int)); 
     for (int page; fscanf(input, "%d", &page) == 1;) {
-        int found_flag = 0;
-        for (int i = 0; i < page_frames_count; i++) {
+        int flag = 0;
+        for (int i = 0; i < pframesCount; i++) {
             if (page == page_table[i]) {
                 hits++;
-                found_flag = 1;
+                flag = 1;
                 break;
             }
         }
 
-        if (found_flag == 0) {
+        if (flag == 0) {
             misses++;
-            unsigned int swap_counter = UINT_MAX;
-            int swap_index = 0;
-            for (int i = 0; i < page_frames_count; i++) {
-                unsigned int cand = page_counter[page_table[i]];
-                if (cand < swap_counter)
-                    swap_counter = cand, swap_index = i;
+            int empty_flag = 0;
+            // to deal with page table sizes greater than AGE_LIMIT, althgout this doesn't really make much sense as a design decision.
+            for (int i = 0; i < pframesCount; i++) {
+                if (page_table[i] == 0) {
+                    page_table[i] = page;
+                    empty_flag = 1;
+                    break;
+                }
             }
-            page_table[swap_index] = page;
+            if (empty_flag == 0) {
+                unsigned int swap_counter = UINT_MAX;
+                int swap_index = 0;
+                for (int i = 0; i < pframesCount; i++) {
+                    unsigned int candidate = PageCounter[page_table[i]];
+                    if (candidate < swap_counter)
+                        swap_counter = candidate, swap_index = i;
+                }
+                page_table[swap_index] = page;
+            }
         }
 
         for (int i = 0; i < MAX_PAGES; i++)
-            page_counter[i] >>= 1;
-        page_counter[page] |= 1 << (AGE_LIM - 1);
+            PageCounter[i] >>= 1;
+        PageCounter[page] |= 1 << (AGE_LIMIT - 1);
     }
 
-    printf("Total pages read:\t%d\n", hits + misses);
-    printf("Total pages hit:\t%d\n", hits);
-    printf("Total pages missed:\t%d\n", misses);
-    printf("Hit ratio:\t\t%.3f%\n", 100.0 * hits / misses);
+    printf("Hit ratio:\t\t%.3f\n", 100.0 * hits / misses);
 
     return 0;
 }
